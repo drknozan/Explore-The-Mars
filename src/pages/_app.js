110a35ef56ea2c01a1c8f442c9b62a9b5a890197
drknozan <drknozan@gmail.com>
2023-02-18 12:06:20 +0300

    feat: progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,35 @@
 import MainLayout from '@/layouts/MainLayout'
 import '@/styles/globals.css'
 import { Orbitron } from '@next/font/google'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import "@/styles/nprogress.css";
 
 const orbitron = Orbitron({ weight: '400', subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    }
+
+    const handleStop = () => {
+      NProgress.done();
+    }
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    }
+  }, [router]);
 
   return (
     <main className={orbitron.className}>
