4158498be825c2825aa54ea50fc0c0c4f3a5969e
drknozan <drknozan@gmail.com>
2023-02-28 12:51:34 +0300

    refactor: image optimization

--- src/components/PhotosItem/index.js
+++ src/components/PhotosItem/index.js
@@ -1,4 +1,5 @@
 import styles from './photosItem.module.css';
+import Image from 'next/image';
 
 const PhotosItem = ({
   id,
@@ -11,7 +12,13 @@ const PhotosItem = ({
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer} onClick={() => showModal(imgSrc)}>
-        <img src={imgSrc} width={400} height={400} alt="RoverPhoto"></img>
+        <Image
+          unoptimized
+          src={imgSrc}
+          width={400}
+          height={400}
+          alt="RoverPhoto"
+        ></Image>
       </div>
       <div className={styles.infos}>
         <p>Rover Name: {roverName}</p>
